4abdb00aef01a44576e2bc66f3cb1fbc4a7b9a37
Quinten Jungblut <quintenjungblut@outlook.com>
2020-04-17 16:29:16 +0000

    Fix Crash in launcher

--- __main__.py
+++ __main__.py
@@ -1,6 +1,10 @@
+import sys
+import os
+
 from qbubbles.__main__ import Main
 
 
 if __name__ == '__main__':
+    sys.path.insert(0, os.path.split(__file__)[0])
     main = Main()
     main.mainloop()
